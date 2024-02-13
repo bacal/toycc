10fbdbb972cf81088d6a18edfd77fecb19e49974
Bacal Mesfin <mesfinb2@vcu.edu>
2024-02-13 16:37:37 -0500

    renamed parser to frontend, moved parser code to module

--- compiler/toycc_frontend/src/parser/error.rs
+++ compiler/toycc_frontend/src/parser/error.rs
@@ -1,33 +1,5 @@
-use std::io::{BufReader, Read, Seek};
-use std::string::ParseError;
 use toycc_report::{Diagnostic, Report, ReportLevel};
-use crate::scanner::token::Token;
-use crate::scanner::{Scanner, };
 use crate::scanner::error::ScannerError;
-
-mod scanner;
-
-pub struct Parser<T: Read + Seek>{
-    scanner: Scanner<T>,
-}
-
-
-impl<T: Read + Seek> Parser<T>{
-    pub fn new(stream: T, stream_name: String, debug: Option<i32>) -> Self{
-        Self{
-            scanner: Scanner::new(stream,stream_name, debug)
-        }
-    }
-
-    pub fn parse(&mut self)-> Result<(),ParserError>{
-        self.scanner.next_token()?;
-        Ok(())
-    }
-
-    fn advance(&mut self) -> Result<Token,ScannerError>{
-        self.scanner.next_token()
-    }
-}
 
 #[derive(Report)]
 pub enum ParserError{
---
+++ compiler/toycc_frontend/src/parser/mod.rs
@@ -0,0 +1,30 @@
+pub mod error;
+
+use std::io::{Read, Seek};
+use crate::parser::error::ParserError;
+use crate::scanner::error::ScannerError;
+use crate::scanner::Scanner;
+use crate::scanner::token::Token;
+
+
+pub struct Parser<T: Read + Seek>{
+    scanner: Scanner<T>,
+}
+
+
+impl<T: Read + Seek> Parser<T>{
+    pub fn new(stream: T, stream_name: String, debug: Option<i32>) -> Self{
+        Self{
+            scanner: Scanner::new(stream,stream_name, debug)
+        }
+    }
+
+    pub fn parse(&mut self)-> Result<(),ParserError>{
+        self.scanner.next_token()?;
+        Ok(())
+    }
+
+    fn advance(&mut self) -> Result<Token,ScannerError>{
+        self.scanner.next_token()
+    }
+}
