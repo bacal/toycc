14b0859b51edeeecb427736f72a3f21ab05d3546
Bacal Mesfin <mesfinb2@vcu.edu>
2024-04-24 16:44:27 -0400

    prints

--- compiler/toycc_frontend/src/parser/ast.rs
+++ compiler/toycc_frontend/src/parser/ast.rs
@@ -98,17 +98,39 @@ pub enum Operator {
     NotEqual,
 }
 
+impl Display for Operator{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f,"{}",match self{
+            Operator::Assign => "=",
+            Operator::Plus => "+",
+            Operator::Minus => "-",
+            Operator::Multiply => "*",
+            Operator::Divide => "/",
+            Operator::Modulo => "%",
+            Operator::Or => "||",
+            Operator::And => "&&",
+            Operator::LessEqual => "<=",
+            Operator::LessThan => "<",
+            Operator::GreaterEqual => ">=",
+            Operator::GreaterThan => ">",
+            Operator::Equal => "==",
+            Operator::NotEqual => "!=",
+        })
+    }
+}
+
 impl Display for Program {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let width = f.width().unwrap_or_default();
         for definition in &self.definitions {
             write!(
                 f,
-                "{:>width$}\n{:>width$}\n{:>width$}",
+                "{:>width$}\n{:>width2$}\n{:>width$}\n",
                 "Program(",
                 definition,
                 ")",
-                width = width + TAB_WIDTH
+                width = width + TAB_WIDTH,
+                width2 = width + (2*TAB_WIDTH),
             )?
         }
         Ok(())
@@ -120,35 +142,66 @@ impl Display for Definition {
         let width = f.width().unwrap_or_default();
         let variant = match self {
             Definition::FuncDef(func_def) => {
-                format!("{:>width$}", func_def, width = width + TAB_WIDTH)
+                format!("{:>width$}", func_def, width = width + TAB_WIDTH +8)
             }
             Definition::VarDef(var_def) => {
-                format!("{:>width$}", var_def, width = width + TAB_WIDTH)
+                format!("{:>width$}", var_def, width = width + TAB_WIDTH+ 7)
             }
         };
-        write!(f, "{:>width$}\n{:>width$}\n", "Definition(", variant)?;
-        write!(f, "{:>width$}", ")", width = width - 1)
+        write!(f, "{:>width$}\n{:>width$}\n", "Definition(", variant, width = width + TAB_WIDTH + 11)?;
+        write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH)
     }
 }
 
 impl Display for FuncDef {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "FuncDef(\n{:4}\n)", self)
+        let width = f.width().unwrap_or_default();
+        write!(f, "{:>width$}\n","FuncDef(", width = width + TAB_WIDTH);
+        write!(f, "{:>width$}\n","Identifier(", width = width + 11 - 2*TAB_WIDTH );
+        write!(f,"{:>width$}\n", self.identifier, width = width + 11 - 4*TAB_WIDTH );
+        write!(f, "{:>width$}\n","),");
+
+        write!(f, "{:>width$}\n","Type(", width = width + 2*TAB_WIDTH + 8);
+        write!(f,"{:>width$}\n", self.toyc_type, width = width + 4*TAB_WIDTH + 6);
+        write!(f, "{:>width$}\n","),", width = width + 2*TAB_WIDTH);
+
+        for def in &self.var_def{
+            write!(f, "{:>width$}\n","VarDef(", width = width + 2*TAB_WIDTH);
+            write!(f,"{:>width$}\n", def, width = width + 4*TAB_WIDTH + 7);
+            write!(f, "{:>width$}\n","),", width = width + 2*TAB_WIDTH );
+        }
+
+        write!(f, "{:>width$}\n","Statement(", width = width + 2*TAB_WIDTH);
+        write!(f,"{:>width$}\n", self.statement, width = width + 4*TAB_WIDTH);
+        write!(f, "{:>width$}\n","),", width = width + TAB_WIDTH + 7);
+
+        write!(f, "{:>width$}\n",")\n", width = width + TAB_WIDTH)
+
     }
 }
 
 impl Display for VarDef {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "VarDef(\n{:4}\n)", self)
+        let width = f.width().unwrap_or_default();
+        write!(f, "{:>width$}\n","VarDef(");
+        write!(f, "{:>width$}\n","Type(", width = width + TAB_WIDTH);
+        write!(f,"{:>width$}\n", self.toyc_type, width = width + 2*TAB_WIDTH);
+        write!(f, "{:>width$}\n","),", width = width + TAB_WIDTH);
+        for def in &self.identifiers{
+            write!(f, "{:>width$}\n","VarDef(", width = width + TAB_WIDTH);
+            write!(f,"{:>width$}\n", def, width = width + 2*TAB_WIDTH);
+            write!(f, "{:>width$}\n","),", width = width + TAB_WIDTH);
+        }
+        write!(f, "{:>width$}\n",")\n")
     }
 }
 
 impl Display for Statement {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let width = f.width().unwrap_or_default();
-        let variant = match self {
+        match self {
             Statement::Expression(e) => {
-                write!(f, "{:>width$}", e, width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", e, width = width + TAB_WIDTH)
             }
 
             Statement::Break => {
@@ -157,7 +210,7 @@ impl Display for Statement {
                     "{:>width$}",
                     "BreakStatement\n",
                     width = width + TAB_WIDTH
-                );
+                )
             }
 
             Statement::BlockState(u, v) => {
@@ -179,7 +232,7 @@ impl Display for Statement {
                     .map(|i| format!("{:>width$}", i, width = width + 2 * TAB_WIDTH))
                     .join(",\n");
                 write!(f, "{:>width$}", v_formatted);
-                write!(f, "{:>width$}", "],\n", width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", "],\n", width = width + TAB_WIDTH)
             }
 
             Statement::IfState(e, s, s1) => {
@@ -190,29 +243,31 @@ impl Display for Statement {
                 if let Some(expr) = s1.as_ref() {
                     write!(f, "{:>width$}", expr, width = width + 2 * TAB_WIDTH);
                 };
-                write!(f, "{:>width$}", "]\n", width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", "]\n", width = width + TAB_WIDTH)
             }
 
             Statement::NullState => {
-                write!(f, "{:>width$}", "NullStatement", width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", "NullStatement\n", width = width + TAB_WIDTH)
             }
 
             Statement::ReturnState(e) => {
                 write!(
                     f,
                     "{:>width$}",
-                    "ReturnStatement(",
+                    "ReturnStatement(\n",
                     width = width + TAB_WIDTH
                 );
-                write!(f, "{:>width$}", e, width = width + 2 * TAB_WIDTH);
-                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH);
+                if let Some(e) = e{
+                    write!(f, "{:>width$}", e, width = width + 2 * TAB_WIDTH);
+                }
+                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH)
             }
 
             Statement::WhileState(e, s) => {
                 write!(
                     f,
                     "{:>width$}",
-                    "WhileStatement(",
+                    "WhileStatement(\n",
                     width = width + TAB_WIDTH
                 );
                 write!(f, "{:>width$}", "[\n", width = width + TAB_WIDTH);
@@ -221,11 +276,11 @@ impl Display for Statement {
                 write!(f, "{:>width$}", "[\n", width = width + TAB_WIDTH);
                 write!(f, "{:>width$}", s.as_ref(), width = width + 2 * TAB_WIDTH);
                 write!(f, "{:>width$}", "]\n", width = width + TAB_WIDTH);
-                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH)
             }
 
             Statement::ReadState(s, s1) => {
-                write!(f, "{:>width$}", "ReadStatement(", width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", "ReadStatement(\n", width = width + TAB_WIDTH);
                 write!(f, "{:>width$}", s, width = width + 2 * TAB_WIDTH);
                 write!(f, "{:>width$}", ",\n", width = width + 2 * TAB_WIDTH);
                 let s1_formatted = match s1 {
@@ -236,37 +291,96 @@ impl Display for Statement {
                     None => format!("{:>width$}", "[]"),
                 };
                 write!(f, "{:>width$}", s1_formatted);
-                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH);
+                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH)
             }
 
             Statement::WriteState(e, e1) => {
                 write!(
                     f,
                     "{:>width$}",
-                    "WriteStatement(",
+                    "WriteStatement(\n",
                     width = width + TAB_WIDTH
                 );
                 write!(f, "{:>width$}", e, width = width + 2 * TAB_WIDTH);
                 write!(f, "{:>width$}", ",\n", width = width + 2 * TAB_WIDTH);
-                let e1_formatted = e1
-                    .unwrap()
-                    .iter()
-                    .map(|f| format!("{:>width$}", f, width = width + 2 * TAB_WIDTH))
-                    .join(",\n");
-
-                write!(f, "{:>width$}", e1_formatted);
-                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH);
+                if let Some(e1) = e1{
+                    for expr in e1{
+                        write!(f, "{:>width$}", expr, width = width + 2 * TAB_WIDTH);
+                    }
+                }
+                write!(f, "{:>width$}", ")\n", width = width + TAB_WIDTH)
             }
 
             Statement::NewLineState => {
                 write!(
                     f,
                     "{:>width$}",
-                    "NewLineStatement",
+                    "NewLineStatement\n",
                     width = width + TAB_WIDTH
-                );
+                )
+            }
+        }
+    }
+}
+
+impl Display for Expression{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let width = f.width().unwrap_or_default();
+        write!(f, "{:>width$}\n","Expression(");
+        match self{
+            Expression::Number(num) => {
+                write!(f, "{:>width$}\n","Identifier(");
+                write!(f,"{:>width$}\n", num, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
+            },
+            Expression::Identifier(id) => {
+                write!(f, "{:>width$}\n","Identifier(");
+                write!(f,"{:>width$}\n", id, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
+            },
+            Expression::CharLiteral(cl) => {
+                let cl =  match cl{
+                    Some(cl) => format!("{cl}"),
+                    None => "".to_string(),
+                };
+                write!(f, "{:>width$}\n","CharLiteral(");
+                write!(f,"{:>width$}\n", cl, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
+            },
+
+            Expression::StringLiteral(s) => {
+                write!(f, "{:>width$}\n","StringLiteral(");
+                write!(f,"{:>width$}\n", s, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
+            },
+            Expression::FuncCall(name, expressions) => {
+                write!(f, "{:>width$}\n","FuncionCall(");
+                write!(f,"{:>width$},\n", name, width = width + 2*TAB_WIDTH);
+                for expr in expressions{
+                    write!(f,"{:>width$},\n", expr, width = width + 2*TAB_WIDTH);
+                }
+                write!(f, "{:>width$}\n",")")
+            },
+            Expression::Expr(op, expra, exprb) => {
+                write!(f, "{:>width$}\n","Expr(");
+                write!(f,"{:>width$},\n", op, width = width + 2*TAB_WIDTH);
+                write!(f,"{:>width$},\n", expra, width = width + 2*TAB_WIDTH);
+                write!(f,"{:>width$},\n", exprb, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
+            },
+            Expression::Not(expr) => {
+                write!(f, "{:>width$}\n","Not(");
+                write!(f,"{:>width$},\n", expr, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
+            },
+            Expression::Minus(expr) => {
+                write!(f, "{:>width$}\n","Minus(");
+                write!(f,"{:>width$},\n", expr, width = width + 2*TAB_WIDTH);
+                write!(f, "{:>width$}\n",")")
             }
         };
+        write!(f, "{:>width$}\n",')')
+
     }
 }
 
